92a25e3a5c51409af1d79b1360f540165061890a
andresdeluna27 <paco_pokemaster12@hotmail.com>
2016-10-17 20:54:38 -0500

    subida

---
+++ subirArch/subir.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head><title>sube archivos</title>
+	</head>
+	<body>
+		<form action="subele.php" method="post" enctype="multipart/form-data">
+			<p>elige un archivo</p><br>
+			<input type="file" name="archivo" id="archivo"></input>
+			<input type="submit" value="Subir archivo"></input>
+		</form>
+	</body>
+</html>
