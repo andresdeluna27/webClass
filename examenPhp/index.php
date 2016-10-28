35704671ed315d9c552557b51c641410ccf5b19e
andresdeluna27 <paco_pokemaster12@hotmail.com>
2016-10-27 20:58:34 -0500

    examen avance

--- examenPhp/index.php
+++ examenPhp/index.php
@@ -4,9 +4,18 @@
 </head>
 <header>
 	<section>logo</section>
-	<a>login</a>
+	<a href="nuevo.php">login</a>
 </header>
 <body>
 	<section>imagen</section>
+	<?php
+	if(isset($_SESSION)){
+		$usu=$_SESSION['usuario'];
+		echo "<p>$usu</p><br><a href=\"#\">logout</a>";
+		##header("location:accede.php");
+	}else{
+		echo "<a href=\"nuevo.php\">registrarse</p>";
+	}
+	?>
 </body>
 </html>
---
+++ examenPhp/insertaU.php
@@ -0,0 +1,25 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors',1 );
+        $servidor="127.0.0.1";
+        $cont="holamundo";
+        $user="root";
+        $bd="blog";
+        $conexion=mysqli_connect($servidor,$user,$cont,$bd);
+        if($conexion){
+             echo "servidor disponible";
+        }else{
+             die("servidor no disponible: ".$conexion->connect_error);
+        }
+	$usuario=$_POST['nuevoUser'];
+	$contrasenia=$_POST['contra'];
+	$nombre=$_POST['nombre'];
+	$correo=$_POST['correo'];
+	$inser=mysqli_query($conexion,"INSERT INTO usuarios VALUES('$usuario','$nombre','$contrasenia','$correo')");
+	if($inser){
+		echo "inserta";
+		header("location:nuevo.php");
+	}else{
+		echo "no inserta";
+	}
+?>
